f9ccfa5d59ab18ecf87785e31f6769d753bdf7f3
ItzManan <manishjainjain197220@gmail.com>
2020-12-16 23:54:49 +0530

    Spaceship moving

--- game.py
+++ game.py
@@ -8,16 +8,33 @@ pygame.display.set_icon(icon)
 ship = pygame.image.load("spaceship.png")
 playerX = 370
 playerY = 480 
+speed = 0
 
-def player():
-    screen.blit(ship, (playerX, playerY))
+def player(x, y):
+    screen.blit(ship, (x, y))
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed = -1
+            if event.key == pygame.K_RIGHT:
+                speed = 1
+        
+        if event.type == pygame.KEYUP:
+            speed = 0
+
     screen.fill((0, 0, 0))
-    player()
+
+    playerX += speed
+    if playerX < -1:
+        playerX =-1
+    elif playerX > 737:
+        playerX = 737
+    player(playerX, playerY)
     pygame.display.update()
 pygame.quit()

135e7ea2adc8fa6d766a6882f9cec503cef2868e
ItzManan <manishjainjain197220@gmail.com>
2020-12-19 00:29:13 +0530

    Completed backend

--- game.py
+++ game.py
@@ -1,67 +1,103 @@
-import pygame
+import pygame as pg
 import random
+from math import sqrt
+from pygame import mixer
 
-pygame.init()
+pg.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pg.display.set_mode((800, 600))
 
-pygame.display.set_caption("Space Invaders")
+pg.display.set_caption("Space Invaders")
 
-icon = pygame.image.load("logo.png")
-pygame.display.set_icon(icon)
+icon = pg.image.load("logo.png")
+pg.display.set_icon(icon)
 
-ship = pygame.image.load("spaceship.png")
+ship = pg.image.load("spaceship.png")
 playerX = 370
 playerY = 480
 speed_player = 0
 
-alien = pygame.image.load("Alien.png")
-alienX = random.randint(0, 760)
-alienY = random.randint(50, 150)
-speed_alien_X = 1
-speed_alien_y = 40
+
+alien = []
+alienX = []
+alienY = []
+speed_alien_X = []
+speed_alien_y = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    alien.append(pg.image.load("Alien.png"))
+    alienX.append(random.randint(0, 760))
+    alienY.append(random.randint(50, 150))
+    speed_alien_X.append(1.5)
+    speed_alien_y.append(40)
 
 
-background = pygame.image.load('Background.jpg')
+background = pg.image.load('Background.jpg')
 
-bullet = pygame.image.load("bullet.png")
+mixer.music.load("background.wav")
+mixer.music.play(-1)
+
+bullet = pg.image.load("bullet.png")
 bulletX = 0
 bulletY = 480
-speed_bullet_y = 2
+speed_bullet_y = 5
 bullet_fire = "ready"
 
+score_value = 0
+font = pg.font.Font("freesansbold.ttf", 32)
+textX = 10
+textY = 10
+
+over = pg.font.Font("freesansbold.ttf", 64)
 
 def player(x, y):
     screen.blit(ship, (x, y))
 
-def aliens(x, y):
-    screen.blit(alien, (x, y))
+def aliens(x, y, i):
+    screen.blit(alien[i], (x, y))
 
 def fire_bullet(x, y):
     global bullet_fire
     bullet_fire = "fire"
     screen.blit(bullet, (x+16, y+10))
 
+def collide(enemyX, enemyY, bulletX, bulletY):
+    distance = sqrt(((enemyX-bulletX)**2)+((enemyY-bulletY)**2))
+    if distance <= 27:
+        return True
+
+def show_score(x, y):
+    score = font.render("Score : "+ str(score_value), True, (0, 255, 0))
+    screen.blit(score, (x, y))
+
+def game_over():
+    over_text = over.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 240))
 
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
         
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                speed_player = -1.2
-            if event.key == pygame.K_RIGHT:
-                speed_player = 1.2
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                speed_player = -2.2
+            if event.key == pg.K_RIGHT:
+                speed_player = 2.2
             
-            if event.key == pygame.K_SPACE:
-                fire_bullet(playerX, bulletY)
+            if event.key == pg.K_SPACE:
+                if bullet_fire is "ready":
+                    bulletX = playerX
+                    fire_bullet(playerX, bulletY)
+                    bullet_sound = mixer.Sound("laser.wav")
+                    bullet_sound.play()
         
-        if event.type == pygame.KEYUP:
-            if speed_player == 1.2 and event.key == pygame.K_RIGHT:
+        if event.type == pg.KEYUP:
+            if speed_player == 2.2 and event.key == pg.K_RIGHT:
                 speed_player = 0
-            if speed_player == -1.2 and event.key == pygame.K_LEFT:
+            if speed_player == -2.2 and event.key == pg.K_LEFT:
                 speed_player = 0
 
     screen.fill((0, 0, 0))
@@ -75,26 +111,53 @@ while running:
     elif playerX > 737:
         playerX = 737
 
-    alienX += speed_alien_X
+    for i in range(num_of_enemies):
+        if alienY[i] > 400:
+            for j in range(num_of_enemies):
+                alienY[j] = 1000
+            game_over()
+            break
 
-    if alienX <= 0:
-        speed_alien_X = 1
-        alienY +=speed_alien_y
+        alienX[i] += speed_alien_X[i]
 
-    elif alienX > 760:
-        speed_alien_X = -1
-        alienY +=speed_alien_y
+        if alienX[i] <= 0:
+            speed_alien_X[i] = 1.5
+            alienY[i] +=speed_alien_y[i]
+
+        elif alienX[i] > 760:
+            speed_alien_X[i] = -1.5
+            alienY[i] +=speed_alien_y[i]
+        
+        collision = collide(alienX[i], alienY[i], bulletX, bulletY)
+
+        if collision:
+            collision_sound = mixer.Sound("explosion.wav")
+            collision_sound.play()
+            bulletY = 480
+            bullet_fire = "ready"
+            score_value +=1
+            
+            alienX[i] = random.randint(0, 760)
+            alienY[i] = random.randint(50, 150)
+            
+        aliens(alienX[i], alienY[i], i)
+
 
     if bullet_fire == "fire":
-        fire_bullet(playerX, bulletY)
+        fire_bullet(bulletX, bulletY)
         bulletY -= speed_bullet_y
-    
+
+        if bulletY < 0:
+            bulletY = 480
+            bullet_fire = "ready"
+
+        
 
     player(playerX, playerY)
 
-    aliens(alienX, alienY)
+    show_score(textX, textY)
 
-    pygame.display.update()
+    pg.display.update()
 
 
-pygame.quit()
+pg.quit()

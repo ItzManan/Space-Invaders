1e93f45c371d59eafeaa8027f2d778f5cb599c36
ItzManan <manishjainjain197220@gmail.com>
2020-12-18 18:03:17 +0530

    Bullet created

--- game.py
+++ game.py
@@ -11,25 +11,37 @@ icon = pygame.image.load("logo.png")
 pygame.display.set_icon(icon)
 
 ship = pygame.image.load("spaceship.png")
-
-alien = pygame.image.load("Alien.png")
-
-background = pygame.image.load('Background.png')
-
 playerX = 370
 playerY = 480
 speed_player = 0
 
+alien = pygame.image.load("Alien.png")
 alienX = random.randint(0, 760)
 alienY = random.randint(50, 150)
 speed_alien_X = 7
 speed_alien_y = 40
+
+
+background = pygame.image.load('Background.png')
+
+bullet = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+speed_bullet_y = 10
+bullet_fire = "ready"
+
 
 def player(x, y):
     screen.blit(ship, (x, y))
 
 def aliens(x, y):
     screen.blit(alien, (x, y))
+
+def fire_bullet(x, y):
+    global bullet_fire
+    bullet_fire = "fire"
+    screen.blit(bullet, (x+16, y+10))
+
 
 running = True
 while running:
@@ -42,6 +54,9 @@ while running:
                 speed_player = -8
             if event.key == pygame.K_RIGHT:
                 speed_player = 8
+            
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
         
         if event.type == pygame.KEYUP:
             if speed_player == 8 and event.key == pygame.K_RIGHT:
@@ -69,6 +84,10 @@ while running:
     elif alienX > 760:
         speed_alien_X = -7
         alienY +=speed_alien_y
+
+    if bullet_fire == "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= speed_bullet_y
     
 
     player(playerX, playerY)
